40d16caf833411ed10004f4c1e35475dfc53e6ea
satis.vishnu <satis.vishnu@gmail.com>
2016-05-28 02:24:06 +0530

    Creating a requirement.txt and a pip_install unix command file to save the dependancies

---
+++ pip_install.sh
@@ -0,0 +1,12 @@
+# This command file can be used to install packages and along with that saving the package name to
+# requirements file.
+#!/usr/bin/env bash
+package_name=$1
+requirements_file=$2
+if [[ -z $requirements_file ]]
+then
+    requirements_file='./requirements.txt'
+fi
+sudo pip install $package_name && pip freeze | grep -i $package_name >> $requirements_file
+
+
