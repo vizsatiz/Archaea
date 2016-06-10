355ea257f677f9512af7ff5ad7ccc7fa6830fdde
satis.vishnu <satis.vishnu@gmail.com>
2016-06-10 15:00:14 +0530

    Adding the parser for parsing the layer structure Json and using that to create the net
    [#120543779]

---
+++ machine_learning/conv_nn/conv_constants.py
@@ -0,0 +1,21 @@
+# Type of Layers
+INPUT_LAYER = 'InputLayer'
+CONVOLUTIONAL_2D_LAYER = 'Conv2DLayer'
+MAX_POOL_2D_LAYER = 'MaxPool2DLayer'
+DROPOUT_LAYER = 'DropoutLayer'
+DENSE_LAYER = 'DenseLayer'
+OUTPPUT_LAYER = ''
+
+# CNN Architecture Params
+LAYER_NAME = 'layer_name'
+LAYER_TYPE = 'layer_type'
+SHAPE = 'shape'
+NUMBER_OF_FILTERS = 'num_filters'
+FILTER_SIZE = 'filter_size'
+LAYER_NONLINEARITY = 'layer_nonlinearity'
+DROPOUT_PIVOT = 'dropout_pivot'
+POOL_SIZE = 'pool_size'
+OUTPUT_UNITS = 'output_units'
+CONV_WINDOW = 'conv_window'
+NUM_UNITS = 'num_units'
+
--- machine_learning/conv_nn/network_builder.py
+++ machine_learning/conv_nn/network_builder.py
@@ -1,4 +1,17 @@
+import cnn_kwargs_builder as kwargs_builder
+from nolearn.lasagne import NeuralNet
+
 
 class cnn_network_builder:
-    def __init__(self):
-        pass
+    def __init__(self, network_architecture):
+        self.network_architecture = network_architecture
+
+    def build(self):
+        """
+        Method parses the network architecture and then builds the network which can be trained using the network trainer
+
+        :return:
+        """
+        [layer_tuples, kwargs] = kwargs_builder.ConvNetArgsBuilder(self.network_architecture).cnn_parameters_parser()
+        cnn_network = NeuralNet(layer_tuples, **kwargs)
+        return cnn_network
