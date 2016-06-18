6775abf15ca867a8f179afe14debdfea538f6052
satis.vishnu <satis.vishnu@gmail.com>
2016-06-19 02:26:06 +0530

    Completing setup.py to make machine_learning module installable
    [#121473079]

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='archaea',
+    version='beta-0.1dev',
+    packages=['machine_learning','machine_learning_tests'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
