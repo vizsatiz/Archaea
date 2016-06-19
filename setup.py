ad8a625d438217339013cbc6ed0bb5568842c6a8
satis.vishnu <satis.vishnu@gmail.com>
2016-06-19 13:31:29 +0530

    Making changes for code distribution by adding setup.py
    [#121801923]

--- setup.py
+++ setup.py
@@ -1,9 +1,34 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='archaea',
-    version='beta-0.1dev',
-    packages=['machine_learning','machine_learning_tests'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
+__version__ = 'alpha-0.1'
+
+setuptools.setup(
+
+    name="archaea",
+    version=__version__,
+    url="https://github.com/archaea-solutions/archaea.git",
+
+    author="vishnu satis",
+    author_email="satis.vishnu@gmail.com",
+
+    description="AI as a Service",
+    long_description=open('README.rst').read(),
+
+    py_modules=['machine_learning', 'machine_learning_tests'],
+    zip_safe=False,
+    platforms='any',
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
 )
